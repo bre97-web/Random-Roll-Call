94c35bad22acb644c68c298c74ddfad269e463f6
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-03-09 14:58:10 +0800

    为list.vue中显示区域增加聚焦环

--- src/components/list.vue
+++ src/components/list.vue
@@ -1,6 +1,6 @@
 <template>
     <ul>
-        <li v-for="e, index in list.value()">{{ e }}</li>
+        <li v-for="e, index in list.value()" :class="{'ring': index == random.getLast()}">{{ e }}</li>
     </ul>
 </template>
 
@@ -13,5 +13,10 @@ const list = useList()
 </script>
 
 <style scoped>
-
+    ul {
+        @apply flex gap-2 flex-wrap;
+    }
+    ul li {
+        @apply px-4 py-2 border rounded-md select-none ring-red-500;
+    }
 </style>
