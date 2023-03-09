dd049c498843ffee61a3031a12b4802ed54486ed
Bre 97 <67639369+bre97-web@users.noreply.github.com>
2023-03-09 15:05:32 +0800

    为List下增加样式
    [Feature] 为List增加删除和增加的功能，并且拥有样式。

--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import App from './App.vue'
  */
 import './assets/index.css'
 
+/**
+ * Google's Material Design Icons
+ */
+import 'material-icons/iconfont/material-icons.css'
+
 import Router from './router/index'
 
 const app = createApp(App)
--- src/views/list.vue
+++ src/views/list.vue
@@ -1,5 +1,14 @@
 <template>
     <div>
+        <ul>
+            
+            <li v-for="e, index in list.value()">
+                <p>{{ e }}</p>
+                <i @click="list.remove(e)" class="material-icons">close</i>
+            </li>
+        </ul>
+
+
         <textarea v-model="key" name="persons" cols="30" rows="10"></textarea>
         <input @click="list.set(key)" type="submit" value="Submit">
     </div>
@@ -20,5 +29,10 @@ watch(list.value(), () => {
 </script>
 
 <style scoped>
-
+    ul {
+        @apply flex flex-col gap-2 flex-wrap;
+    }
+    ul li {
+        @apply px-4 py-2 border rounded-md select-none flex justify-between items-center;
+    }
 </style>
